91f8ed5f2b3ad1f073bc0e15d8e8f9da6dc130a4
lorenzodisanto <lore.disanto95@gmail.com>
2024-10-11 10:42:17 +0200

    feat: add data and clean code

--- src/components/home/OurServices.vue
+++ src/components/home/OurServices.vue
@@ -1,42 +1,68 @@
 <script>
 export default {
-  name: "OurServices",
+  data() {
+    return {
+      services: [
+        {
+          name: "Igiene professionale (GBT)",
+          description:
+            "Microscopio e GBT (Guided Biofilm Therapy): una coppia vincente!",
+          image: ".",
+        },
+        {
+          name: "Ortodonzia",
+          description:
+            "Ci occupiamo di Ortodonzia intercettiva, tradizionale ed invisibile.",
+          image: ".",
+        },
+        {
+          name: "Gnatologia ed Elettromiografia di superficie",
+          description:
+            "Digrigni o serri i denti? Soffri di mal di testa o dolori cervicali? ",
+          image: ".",
+        },
+        {
+          name: "Chirurgia orale e Implantologia",
+          description:
+            "Offriamo servizi di chirurgia orale ed implantologia computer guidata ed avanzata ",
+          image: ".",
+        },
+        {
+          name: "Radiologia",
+          description:
+            "I servizi di radiologia digitale che offriamo (Ortopantomografia, endorale e TAC 3D)",
+          image: ".",
+        },
+        {
+          name: "Scansione digitale",
+          description:
+            "Lo scanner intraorale è un dispositivo per creare un modello digitale delle arcate dentali. ",
+          image: ".",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <template>
   <section>
     <div class="container border">
-      <h2 class="text-center">I nostri servizi</h2>
-      <div class="row g-3 p-5">
-        <div class="col-4">
+      <h2 class="text-center pt-4">I nostri servizi</h2>
+      <div class="row g-3 p-4">
+        <div class="col-4" v-for="service in this.services">
           <div class="card">
-            <div class="card-body">IGIENE PROFESSIONALE (GBT)</div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card"><div class="card-body">ORTODONZIA</div></div>
-        </div>
-        <div class="col-4">
-          <div class="card">
+            <img
+              src="../../../public/logo/DiSanto.logotipo-02.svg"
+              class="card-img-top"
+              alt="..."
+            />
             <div class="card-body">
-              GNATOLOGIA ed ELETTROMIOGRAFIA DI SUPERFICIE
+              <h5 class="card-title">{{ service.name }}</h5>
+              <p class="card-text">
+                {{ service.description }}
+              </p>
             </div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card">
-            <div class="card-body">CHIRURGIA ORALE E IMPLANTOLOGIA</div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card">
-            <div class="card-body">RADIOLOGIA</div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card">
-            <div class="card-body">SCANSIONE DIGITALE</div>
           </div>
         </div>
       </div>
