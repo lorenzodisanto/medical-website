fd5b1df3e101a4d0bfa9098bbb2a3d8cb8eeb98f
lorenzodisanto <lore.disanto95@gmail.com>
2024-10-14 11:48:50 +0200

    feat: import fontawesome

--- src/main.js
+++ src/main.js
@@ -6,4 +6,21 @@ import { router } from "./router";
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
 
-createApp(App).use(router).mount("#app");
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faAngleLeft, faAngleRight);
+
+const app = createApp(App);
+
+app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount("#app");
