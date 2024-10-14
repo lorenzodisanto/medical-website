8bcff879c1de10fadcbcce891e66441a0d1ca001
lorenzodisanto <lore.disanto95@gmail.com>
2024-10-14 15:45:11 +0200

    feat: add content in welcome component

--- src/components/home/Welcome.vue
+++ src/components/home/Welcome.vue
@@ -6,15 +6,13 @@ export default {
 
 <template>
   <section>
-    <div class="border d-flex">
-      <div class="w-50">
-        <img
-          src="../../../public/logo/DiSanto.logotipo.payoff.verticale-03.svg"
-          alt=""
-        />
+    <div class="container-fluid welcome-content">
+      <div class="welcome-image py-5">
+        <img src="../../assets/sala-attesa.jpg" alt="" />
       </div>
-      <div class="w-50">
-        <p class="p-5 fs-5">
+      <div class="p-5">
+        <h3 class="mb-4">Benvenuti nel nostro centro!</h3>
+        <p class="fs-5 welcome-text">
           Di Santo Odontoiatria Specialistica è un centro odontoiatrico a Telese
           Terme (BN). Lavoriamo con passione, con precisione grazie ai nostri
           Microscopi che usiamo in tutti i campi dell’odontoiatria e sempre
@@ -37,8 +35,22 @@ export default {
 section {
   background-color: $bone;
   color: black;
-}
-.border {
-  border: 1px dashed black;
+  .welcome-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .welcome-image {
+      text-align: end;
+      img {
+        width: 80%;
+        border-radius: 10px;
+      }
+    }
+    .welcome-text {
+      width: 80%;
+      text-align: justify;
+    }
+  }
 }
 </style>
