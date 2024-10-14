335b43935b1175b7d5db4dd4293e017cc78e8829
lorenzodisanto <lore.disanto95@gmail.com>
2024-10-14 17:18:28 +0200

    style: service card

--- src/components/home/OurServices.vue
+++ src/components/home/OurServices.vue
@@ -8,14 +8,23 @@ export default {
       store,
     };
   },
+  methods: {
+    buildImagePath(imageName) {
+      const imageUrl = new URL(
+        "../../assets/serviceImg/" + imageName,
+        import.meta.url
+      );
+      return imageUrl.href;
+    },
+  },
 };
 </script>
 
 <template>
   <section>
     <div class="container">
-      <h2 class="text-center pt-4">I nostri servizi</h2>
-      <div class="row g-3 p-4">
+      <h2 class="text-center pt-5">I nostri servizi</h2>
+      <div class="row g-3 py-5">
         <div class="col-12 col-md-6 col-xl-4" v-for="service in store.services">
           <router-link
             :to="{
@@ -23,12 +32,14 @@ export default {
               params: { slug: service.slug },
             }"
             ><div class="card h-100">
-              <img
-                src="../../../public/logo/DiSanto.logotipo-01.svg"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
+              <div class="img-container">
+                <img
+                  :src="buildImagePath(service.image)"
+                  alt=""
+                  class="card-img-top"
+                />
+              </div>
+              <div class="card-body service-description">
                 <h5 class="card-title">{{ service.name }}</h5>
                 <p class="card-text">
                   {{ service.description }}
@@ -42,9 +53,25 @@ export default {
   </section>
 </template>
 
-<style scoped>
-.border {
-  border: 1px dashed black;
+<style lang="scss" scoped>
+@use "../../styles/partials/variables.scss" as *;
+.card {
+  .img-container {
+    overflow: hidden;
+    .card-img-top {
+      transition: 0.7s;
+    }
+  }
+  .service-description {
+    h5 {
+      font-weight: 600;
+    }
+    background-color: $fresh-teal;
+  }
+}
+.card:hover img {
+  transform: scale(1.1);
+  transition: 0.7s;
 }
 a {
   text-decoration: none;
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,7 @@ export const store = reactive({
       name: "Igiene professionale (GBT)",
       description:
         "Microscopio e GBT (Guided Biofilm Therapy): una coppia vincente!",
-      image: ".",
+      image: "gbt.jpg",
       slug: "igiene-professionale-gbt",
       details:
         "Microscopio e GBT (Guided Biofilm Therapy): una coppia vincente!Certificati dalla Swiss Dental Academy, grazie al Microscopio Operatorio e al nuovo protocollo di Igiene Orale GBT, la seduta di igiene professionale è sinonimo di esperienza di benessere.Nel nuovo e moderno protocollo GBT di igiene professionale dell’azienda Svizzera EMS tutto ruota attorno alla rimozione del biofilm, delle macchie e del tartaro con tecnologie innovative e all’avanguardia, preservando con delicatezza la superficie dei denti e delle gengive. La GBT è un protocollo clinico indolore, rapido e sicuro che garantisce uno standard di qualità elevato ed uniforme… tutto rigorosamente al Microscopio!",
@@ -16,7 +16,7 @@ export const store = reactive({
       name: "Ortodonzia",
       description:
         "Ci occupiamo di Ortodonzia intercettiva, tradizionale ed invisibile.",
-      image: ".",
+      image: "ortodonzia.jpeg",
       slug: "ortodonzia",
       details:
         "Ci occupiamo di Ortodonzia intercettiva, tradizionale ed invisibile. Riuscire a stabilire un morso corretto è fondamentale per l’estetica ma soprattutto per la tua salute della bocca e del corpo. Tipicamente le terapie ortodontiche vengono realizzate da bambini, riuscendo così a intercettare problematiche di sviluppo fin dai primi anni di vita. Così facendo guidiamo un armonico accrescimento delle basi ossee e degli elementi dentali dei nostri piccoli pazienti. E’ possibile però intervenire anche da adulti, con tecniche e tempistiche diverse che, ad oggi, ci regalano un ventaglio di soluzioni terapeutiche per poter curare tutti i pazienti di tutte le fasce d’età.",
@@ -25,7 +25,7 @@ export const store = reactive({
       name: "Gnatologia ed Elettromiografia di superficie",
       description:
         "Digrigni o serri i denti? Soffri di mal di testa o dolori cervicali? ",
-      image: ".",
+      image: "gnatologia.jpg",
       slug: "gnatologia-ed-elettromiografia-di-superficie",
       details:
         "Digrigni o serri i denti? Soffri di mal di testa o dolori cervicali? Dolori all’articolazione temporo-mandibolare? Potrebbe trattarsi di malocclusione dentale. Con “Kinelock Dental”, un dispositivo di ultima generazione, è possibile valutare, con un esame veloce, confortevole e senza alcun fastidio, il tuo bilanciamento occlusale. Grazie al suo algoritmo esclusivo e la sua intelligenza artificiale, “Kinelock Dental” rilascia in pochi minuti un referto diagnostico che permette di individuare la corretta strategia di intervento per risolvere eventuali problemi legati alla malocclusione dentale.",
@@ -35,7 +35,7 @@ export const store = reactive({
       name: "Chirurgia orale e Implantologia",
       description:
         "Offriamo servizi di chirurgia orale ed implantologia computer guidata ed avanzata ",
-      image: ".",
+      image: "chirurgia.jpg",
       slug: "chirurgia-orale-e-implantologia",
       details:
         "Offriamo servizi di chirurgia orale ed implantologia computer guidata ed avanzata (Centro di Eccellenza in Implantologia Digitale Lyra Etk) per riabilitazioni semplici e complesse grazie all’utilizzo di tecniche moderne e attrezzature all’avanguardia per garantire comfort e sicurezza ai nostri pazienti.",
@@ -45,7 +45,7 @@ export const store = reactive({
       name: "Radiologia",
       description:
         "I servizi di radiologia digitale che offriamo: Ortopantomografia, endorale e TAC 3D",
-      image: ".",
+      image: "radiologia.JPG",
       slug: "radiologia",
       details:
         "I servizi di radiologia digitale che offriamo (Ortopantomografia, endorale e TAC 3D) abbassano l’esposizione ai raggi X e aumentano la qualità delle immagini ottenute e sono un ausilio diagnostico utilissimo e spesso indispensabile.",
@@ -54,7 +54,7 @@ export const store = reactive({
       name: "Scansione digitale",
       description:
         "Lo scanner intraorale è un dispositivo per creare un modello digitale delle arcate dentali. ",
-      image: ".",
+      image: "scanner.jpg",
       slug: "scansione-digitale",
       details:
         "Lo scanner intraorale è un dispositivo che esegue una scansione del cavo orale per creare un modello digitale delle arcate dentali. Sostituisce le tradizionali impronte e permette di eseguire una scansione digitale e tridimensionale delle arcate dentarie del paziente. L’operazione si svolge semplicemente passando lo scanner, simile a una penna, all’interno della bocca in prossimità dei denti. Questa scansione ha come risultato un rendering 3D che può essere quindi essere esplorato in tutte le dimensioni. I dettagli anatomici che rivela sono molto più accurati rispetto alla tradizionale impronta.",
