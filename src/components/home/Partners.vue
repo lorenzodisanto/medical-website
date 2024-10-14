e77ae08fbb0bbf564c95fe21cebe400e3937bc49
lorenzodisanto <lore.disanto95@gmail.com>
2024-10-14 15:18:08 +0200

    feat: add partners image

--- src/components/home/Partners.vue
+++ src/components/home/Partners.vue
@@ -1,28 +1,68 @@
 <script>
 export default {
   name: "Partners",
+  data() {
+    return {
+      partners: [
+        {
+          img: "lumineers.png",
+          link: "https://lumineersitalia.it/",
+        },
+        {
+          img: "gbt.png",
+          link: "https://www.ems-dental.com/it",
+        },
+        {
+          img: "blancone.png",
+          link: "https://www.blancone.eu/it/",
+        },
+        {
+          img: "lyraetk.png",
+          link: "https://www.lyraetk.com/it/pagina-iniziale/",
+        },
+      ],
+    };
+  },
+  methods: {
+    buildImagePath(imageName) {
+      const imageUrl = new URL(
+        "../../assets/partnersImg/" + imageName,
+        import.meta.url
+      );
+      return imageUrl.href;
+    },
+  },
 };
 </script>
 
 <template>
   <section>
-    <div class="container-fluid border">
-      <div class="row p-5 text-center">
-        <div class="col">Centro di eccellenza LUMINEERS</div>
-        <div class="col">
-          Centreo di eccellenza di implantologia digitale LYRA ETK
+    <div class="container-fluid">
+      <div class="row p-3 text-center">
+        <div class="col" v-for="partner in partners">
+          <a :href="partner.link" target="_blank">
+            <div class="partner-box">
+              <img :src="buildImagePath(partner.img)" alt="" />
+            </div>
+          </a>
         </div>
-        <div class="col">
-          Centreo di eccellenza GBT dalla “SWISS DENTAL ACADEMY”
-        </div>
-        <div class="col">Centro sbiancamento dentale BLANCONE</div>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
-.border {
-  border: 1px dashed black;
+.col {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.partner-box {
+  padding: 0px 85px;
+  transition: 0.7s;
+}
+.partner-box:hover {
+  transform: scale(1.1);
+  transition: 0.7s;
 }
 </style>
